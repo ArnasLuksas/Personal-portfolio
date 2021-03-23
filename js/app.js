f660e9ca548884557d4a532c531d826aafdddff0
Arnas <arnasluksas@gmail.com>
2021-03-23 12:43:12 +0000

    little update in app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
-//toggle mobile navigation menu
+//selecting items from DOM
 const burgerLogo = document.querySelector('.lines');
 const mobNav = document.querySelector('.nav-list');
 
-//add or remove show class when clicking on burger logo
-burgerLogo.addEventListener('click', (e) => {
-  mobNav.classList.toggle('show');
+//toogle mobile nav
+burgerLogo.addEventListener('click', () => {
+  showNav();
 });
+const showNav = () => {
+  mobNav.classList.toggle('show');
+};
 
-//remove show class when clicking on nav menu li
+//after selecting item from list, we close the mobile nav
 mobNav.addEventListener('click', (e) => {
+  closeNav(e);
+});
+const closeNav = (e) => {
   const item = e.target.innerHTML;
   if (item) {
     mobNav.classList.remove('show');
   }
-});
+};
